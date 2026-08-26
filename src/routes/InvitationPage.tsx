307b52ca76944443c9d3e5d09b1bec74445e315d
Teuku Fandi Ahmad <tfandiahmad32@gmail.com>
2026-08-26 16:10:40 +0700

    gitu dah ngubah link doang

--- src/routes/InvitationPage.tsx
+++ src/routes/InvitationPage.tsx
@@ -119,7 +119,7 @@ export const InvitationPage: React.FC = () => {
             <div>
               Website dibangun oleh{' '}
               <a
-                href="https://andyys-portfolio.vercel.app/"
+                href="https://andyys-dev.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="font-medium text-gold-700 hover:text-gold-600 underline underline-offset-2 transition-colors"
